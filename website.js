66998adbd810efbff874c09bdf916219a1ab37c3
ArmaVoss <aj.vossoughi@gmail.com>
2024-04-11 20:25:20 -0400

    worked on about page

--- website.js
+++ website.js
@@ -2,6 +2,13 @@ let intro = document.querySelector('.intro');
 
 window.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
-        intro.style.top = '-100vh'; // Use style.top instead of computedStyleMap
-    },500); // Add missing semicolon and specify the delay correctly
+        intro.style.top = '-100vh';
+       
+    },500); 
 });
+
+
+
+
+
+
